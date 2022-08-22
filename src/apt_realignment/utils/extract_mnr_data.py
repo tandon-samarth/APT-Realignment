76587dcf23e25a0c0db879db5265737402ecb255
Samarth <samarth.tandon@tomtom.com>
2022-08-22 11:56:03 +0530

    Feature Added for polygon as arr and nearest polygon in geo utils

--- src/apt_realignment/utils/extract_mnr_data.py
+++ src/apt_realignment/utils/extract_mnr_data.py
@@ -181,8 +181,8 @@ def download_apt_elements(shape_file, out_path):
 
 
 if __name__ == '__main__':
-    mnr_database = ExtractMNRData(country_code='_2022_06_010_nam_usa_uca')
+    mnr_database = ExtractMNRData(country_code='_2022_06_012_nam_usa_utx')
     mnr_database.connect_to_server()
-    out_path = '/mnt/c/Users/tandon/OneDrive - TomTom/Desktop/tomtom/Workspace/01_Rooftop_accuracy/BFP_Analysis_USA/data/data/California'
+    out_path = '/mnt/c/Users/tandon/OneDrive - TomTom/Desktop/tomtom/Workspace/01_Rooftop_accuracy/BFP_Analysis_USA/data/data/Texas'
     mnr_apt_df = mnr_database.extract_apt_addresses_data()
-    mnr_database.save_dataframe_as_shpfile(mnr_apt_df, out_path, filename='APT_2022_06_010_nam_usa_uca.shp')
+    mnr_database.save_dataframe_as_shpfile(mnr_apt_df, out_path, filename='APT_2022_06_012_nam_usa_utx.shp')
